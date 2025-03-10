adc4ebd5f6c4c1b10bc1b6353639b6bb8c6a1fc2
Pooja <poojabishu96@gmail.com>
2025-03-10 23:39:34 +0530

    changes

--- src/config/db.js
+++ src/config/db.js
@@ -8,7 +8,7 @@ const connectDB = async () => {
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            dbName: 'order_processing' // Set database name here
+            dbName: 'order_processing'
         });
         console.log('MongoDB Connected');
     } catch (error) {
